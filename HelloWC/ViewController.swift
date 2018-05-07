c1b129b3f4a7e1eec2931b81df3480eca86441d9
eeam <eeam@EEAMs-iMac.local>
2018-05-07 23:09:20 +0800

    first commit

---
+++ HelloWC/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HelloWC
+//
+//  Created by Flower on 2018/5/2.
+//  Copyright © 2018年 Flower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(sender:UIButton){
+        let alertController = UIAlertController(title:"Welcome", message: "Hello", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+
+}
+
+
+
+}
+
