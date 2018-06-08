92d333e0f7d94207ff464d71653e81fc7b551a76
Flower <flower@120-125-113-130.TANET-KM-IP.km.edu.tw>
2018-06-08 17:00:55 +0800

    CH9 custom cell

---
+++ MyFood/MyFood/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  MyFood
+//
+//  Created by Flower on 2018/6/8.
+//  Copyright © 2018年 KFlower. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var localtionLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var myImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
